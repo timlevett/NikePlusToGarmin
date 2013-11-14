b55971f45df64a51d21a3130e41ae9869ff5cb51
timlevett <tim.levett@gmail.com>
2013-11-13 21:31:07 -0600

    Fixed temp code so it compiles

--- src/main/java/com/timlevett/nptg/transformer/TransformNikePlusToGPX.java
+++ src/main/java/com/timlevett/nptg/transformer/TransformNikePlusToGPX.java
@@ -4,12 +4,13 @@ import com.timlevett.nptg.model.Activity;
 import com.timlevett.nptg.model.GPSData;
 import com.topografix.gpx._1._0.Gpx;
 
+//TODO : FINISH ME
 public class TransformNikePlusToGPX {
 	public Gpx toGPX(Activity npActivity, GPSData npGPSData) {
 		
 		Gpx gpx = new Gpx();
 		
-		gpx.set
+		//gpx.set
 		
 		return null;
 	}
